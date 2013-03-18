f6b5b6acc221bab60d3e6730cd5baa76e42b40d0
ycombinator <ycombinator@gmail.com>
2013-03-18 09:27:25 -0700

    init checkin.

---
+++ yelp_restaurant_extractor.js
@@ -0,0 +1,25 @@
+var fs = require('fs'),
+    yelpFetcher = require('./lib/yelp_fetcher'),
+    yelpCsvFormatter = require('./lib/yelp_csv_fetcher'),
+    fileWriter = require('./lib/file_writer')
+
+var writer = fileWriter.FileWriter({
+    file: __dirname + 'var/yelp.csv'
+})
+    
+var formatter = yelpCsvFormatter.YelpCsvFormatter({
+    writer: writer
+})
+        
+var fetcher = yelpFetcher.YelpFetcher({
+    apiConsumerKey: process.env.YELP_API_CONSUMER_KEY,
+    apiConsumerSecret: process.env.YELP_API_CONSUMER_SECRET,
+    apiToken: process.env.YELP_API_TOKEN,
+    apiTokenSecret: process.env.YELP_API_TOKEN_SECRET,
+    formatter: formatter
+})
+
+fetcher.fetch({
+    location: 'San Francisco, CA',
+    category: 'restaurants'
+})
